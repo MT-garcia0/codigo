a6be0fbb041082fe79f0c1aaef9a1522ff7f89d8
MT-garcia0 <matheus.g.oliveira17@aluno.senai.br>
2025-02-13 13:01:51 -0300

    Add files via upload

---
+++ Kit.bat
@@ -0,0 +1,20 @@
+mkdir kleber 
+pause 
+rmdir kleber
+mkdir cariane
+pause
+cd cariane
+echo. >pol.txt
+copy pol.txt cariane
+pause
+cd ..
+mkdir cosielo
+move cariane\pol.txt cosielo\polmovido.txt
+pause
+cd cosielo 
+rename polmovido.txt pol.txt
+pause
+del pol.txt
+echo bom dia de novo é quinta feira. > balestrim.txt
+type balestrim.txt 
+pause
